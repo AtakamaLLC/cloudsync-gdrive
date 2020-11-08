3c6bfee81d41ecea4a49d1c2d91d76e988290ea1
Michael Krebs <michael@vidaidentity.com>
2020-11-08 17:02:26 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cloudsync-gdrive',
+    version='1.0.9a',
+    packages=['tests'],
+    url='',
+    license='',
+    author='Atakama',
+    author_email='',
+    description=''
+)
